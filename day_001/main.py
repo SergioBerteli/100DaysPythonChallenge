a4e3a40e624ad2431a4ca4863a7a33fa5f35bc9f
Sergio Berteli Batista Filho <sergio@univali.br>
2023-02-14 18:43:28 -0300

    Mudanças no main.py do primeiro dia
    foi colocado o algoritmo no arquivo main.py

--- day_001/main.py
+++ day_001/main.py
@@ -1,1 +1,7 @@
-print("Meu primeiro dia!")
+if __name__ == '__main__':
+    print('Welcome to the Band Name Generator')
+    city = input('What\'s name of the city you grew up in?\n')
+    pet = input('What\'s your pet\'s name?\n')
+    band_name =  ' '.join([city, pet])
+    print(f'Your band name could be {band_name}')
+
